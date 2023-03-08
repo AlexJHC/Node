a07f69e3ee43b97a0fc922f0646d5998dd55b693
AlexJHC <85096994+AlexJHC@users.noreply.github.com>
2023-03-09 01:15:53 +0200

    added create new user && sign in methods

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,15 +1,37 @@
-import prisma from 'prisma/db'
+import prisma from '../../prisma/db'
 import { type Request, type Response } from 'express'
-import { hashPasswords } from '../modules/auth'
+import { comparePasswords, createJWT, hashPasswords } from '../modules/auth'
 
 export const createNewUser = async (
   req: Request<{ body: { username: string; password: string } }>,
   res: Response
 ) => {
+  console.log(1)
   const user = await prisma.user.create({
     data: {
       username: req.body.username,
       password: await hashPasswords(req.body.password),
     },
   })
+
+  const token = createJWT(user)
+  res.json({ token })
+}
+
+export const signIn = async (req: Request, res: Response) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  })
+  const isValid = await comparePasswords(req.body.password, user.password)
+
+  if (!isValid) {
+    res.status(401)
+    res.json({ message: 'Not valid password' })
+    return
+  }
+
+  const token = createJWT(user)
+  res.json({ token })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,24 @@
-import express, {type Request, type Response} from 'express'
-import router from './router';
+import express, { type Request, type Response } from 'express'
+import router from './router'
 import morgan from 'morgan'
-import {protect} from './modules/auth';
+import { protect } from './modules/auth'
+import { createNewUser, signIn } from './handlers/users'
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200)
-  res.json({message: 'hello'})
+  res.json({ message: 'hello' })
 })
 
 app.use('/api', protect, router)
+
+app.use('/user', createNewUser)
+
+app.use('/signIn', signIn)
 
 export default app
